304462d7a7cb9bc422c475045aa665bd555eacdf
Felipe <felipebruckmann@hotmail.com.br>
2020-10-21 08:30:25 -0300

    fixing the imports and implements onInit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PhotoService } from './photos/photo.service';
+import { Photo } from './photos/photo.model';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-	photos: Object[] = [];
+	photos: Photo[] = [];
 
-	constructor(private photoService: PhotoService) {
+	constructor(private photoService: PhotoService) {}
+
+	ngOnInit() {
 		this.photoService.listFromUser('flavio').subscribe((photos) => {
 			this.photos = photos;
 		})
 	}
+
 	
 }
--- src/app/photos/photo.service.ts
+++ src/app/photos/photo.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Photo } from './photo.model'
 
 const API = 'http://localhost:3000/'
 
@@ -13,7 +14,7 @@ export class PhotoService {
   constructor(private http: HttpClient) { }
 
   listFromUser(userName) {
-    return this.http.get<Object[]>(API + `${userName}/photos`)	
+    return this.http.get<Photo[]>(API + `${userName}/photos`)	
   }
   
 }
